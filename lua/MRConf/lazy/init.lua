9bddff44fddc499742061b7823b824412ceff478
Maxime Regeard <regeard@apc.in2p3.fr>
2024-05-14 23:03:17 +0200

    trouble

--- lua/MRConf/lazy/init.lua
+++ lua/MRConf/lazy/init.lua
@@ -3,13 +3,5 @@ return {
         'nvim-lua/plenary.nvim',
         name = "plenary"
     },
-    {
-        "folke/trouble.nvim",
-        config = function()
-            require("trouble").setup {
-                icons = false,
-            }
-        end
-    },
     'eandrju/cellular-automaton.nvim',
 }
