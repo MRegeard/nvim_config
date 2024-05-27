0d759fae882fa902ac9d08e03c4846e9d4efd231
Maxime Regeard <regeard@apc.in2p3.fr>
2024-05-27 12:42:44 +0200

    all file replace remap

--- lua/MRConf/remap.lua
+++ lua/MRConf/remap.lua
@@ -5,3 +5,6 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>//gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>cs", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
