3947596750aba58e2920a8c9f05f262a7fb1e82b
Maxime Regeard <regeard@apc.in2p3.fr>
2026-01-02 18:16:50 +0100

    add zig fmt on save

--- lua/MRConf/lazy/conform.lua
+++ lua/MRConf/lazy/conform.lua
@@ -1,12 +1,32 @@
 return {
-    'stevearc/conform.nvim',
-    opts = {},
-    config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-                lua = { "stylua" },
-                go = { "gofmt" }
-            }
-        })
-    end
+  "stevearc/conform.nvim",
+  opts = {},
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        go = { "gofmt" },
+        zig = { "zigfmt" }, -- add zig
+      },
+      format_on_save = {
+        timeout_ms = 2000,
+        lsp_fallback = false, -- don't use zls formatting
+      },
+    })
+
+    -- Define zig formatter (runs `zig fmt <file>` in-place)
+    conform.formatters.zigfmt = {
+      command = "zig",
+      args = { "fmt", "$FILENAME" },
+      stdin = false,
+    }
+
+    -- Optional: manual command
+    vim.api.nvim_create_user_command("ZigFmt", function()
+      conform.format({ async = false, lsp_fallback = false })
+    end, {})
+  end,
 }
+
