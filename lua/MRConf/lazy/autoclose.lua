e6607a7842910ea46690589fd35e87dbacb6c52e
Maxime Regeard <regeard@apc.in2p3.fr>
2026-05-21 14:31:35 +0200

    disable autoclose of ' in markdown file

--- lua/MRConf/lazy/autoclose.lua
+++ lua/MRConf/lazy/autoclose.lua
@@ -1,11 +1,11 @@
 return {
     "m4xshen/autoclose.nvim",
-        config = function()
-            require("autoclose").setup({
-                keys = {
-                    ["$"] = { escape = true, close = true, pair = "$$", disabled_filetypes = {} },
-                    ["'"] = { escape = true, close = true, pair = "''", disabled_filetypes = {"tex"} },
-                },
-            })
-        end,
+    config = function()
+        require("autoclose").setup({
+            keys = {
+                ["$"] = { escape = true, close = true, pair = "$$", disabled_filetypes = {} },
+                ["'"] = { escape = true, close = true, pair = "''", disabled_filetypes = { "tex", "markdown" } },
+            },
+        })
+    end,
 }
