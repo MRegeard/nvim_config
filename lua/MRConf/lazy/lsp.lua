0299c64760553dc542cb0606e31d4b0ea03beef8
Maxime Regeard <regeard@apc.in2p3.fr>
2026-07-13 18:40:59 +0200

    tab

--- lua/MRConf/lazy/lsp.lua
+++ lua/MRConf/lazy/lsp.lua
@@ -29,12 +29,10 @@ return {
             ensure_installed = {
                 "lua_ls",
                 "rust_analyzer",
-                "pylsp",
                 "zls",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
-
                     require("lspconfig")[server_name].setup {
                         capabilities = capabilities
                     }
@@ -54,7 +52,6 @@ return {
                     })
                     vim.g.zig_fmt_parse_errors = 0
                     vim.g.zig_fmt_autosave = 0
-
                 end,
 
                 ["lua_ls"] = function()
@@ -63,7 +60,7 @@ return {
                         capabilities = capabilities,
                         settings = {
                             Lua = {
-				    runtime = { version = "Lua 5.1" },
+                                runtime = { version = "Lua 5.1" },
                                 diagnostics = {
                                     globals = { "vim", "it", "describe", "before_each", "after_each" },
                                 }
